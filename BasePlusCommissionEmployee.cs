9fdafa40d9f29914cdc3853e6aaf6a817cb94da7
Oleksandr Radkevych <aradevich78@gmail.com>
2023-10-14 22:38:19 +0500

    Add Class BasePlusComissionEmployee

---
+++ BasePlusCommissionEmployee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_012_Inheritance
+{
+    public class BasePlusCommissionEmployee
+    {
+             public string FirstName { get; }
+            public string LastName { get; }
+            public string SocialSecurityNumber { get; }
+
+            private decimal grossSales;
+            private decimal commissionRate;
+            private decimal baseSalary;
+
+            public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecurityNumber, decimal grossSales, decimal commissionRate, decimal baseSalary)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                SocialSecurityNumber = socialSecurityNumber;
+                GrossSales = grossSales;
+                CommissionRate = commissionRate;
+                BaseSalary = baseSalary;
+            }
+
+            public decimal GrossSales
+            {
+                get
+                {
+                    return grossSales;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value),
+                            value, $"{nameof(GrossSales)} must be >= 0");
+                    }
+                    grossSales = value;
+                }
+            }
+
+            public decimal CommissionRate
+
+            {
+                get { return commissionRate; }
+                set
+                {
+                    if (value <= 0 || value >= 1)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(CommissionRate)} must be > 0 and < 1");
+                    }
+                    commissionRate = value;
+                }
+            }
+       
+        public decimal BaseSalary
+
+        {
+            get { return baseSalary; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(CommissionRate)} must be >= 0");
+                }
+                baseSalary = value;
+            }
+        }
+
+        public decimal Earnings() => baseSalary + (grossSales * commissionRate);
+
+            public override string ToString() =>
+                $"Commission employee: {FirstName} {LastName}\n"
+                + $"Social security number: {SocialSecurityNumber}\n"
+                + $"Gross sales: {grossSales}\n"
+                + $"Comission rate: {commissionRate}\n"
+                + $"Base salary: {baseSalary}\n";
+
+
+
+
+        }
+
+}
---
+++ CommissionEmployeeTest.cs
@@ -0,0 +1,44 @@
+﻿namespace NET_012_Inheritance
+{
+    internal class CommissionEmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            var employee = new CommissionEmployee("Sue", "Jones", "222-222-2222", 10000.0M, .06M);
+         
+            Console.WriteLine("Employee information obtained by properties and methods: \n");
+            Console.WriteLine($"First name is {employee.FirstName}");
+            Console.WriteLine($"Last name is {employee.LastName}");
+            Console.WriteLine($"Social security number {employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales {employee.GrossSales:C}");
+            Console.WriteLine($"Commission rate {employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {employee.Earnings():C}");
+
+            employee.GrossSales = 5000.0M;
+            employee.CommissionRate = 0.1M;
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString() method:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings {employee.Earnings():C}\n");
+
+            //
+            var bpemployee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-333-3333", 5000.0M, .04M, 300);
+            Console.WriteLine("BP Employee information obtained by properties and methods: \n");
+            Console.WriteLine($"First name is {bpemployee.FirstName}");
+            Console.WriteLine($"Last name is {bpemployee.LastName}");
+            Console.WriteLine($"Social security number {bpemployee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales {bpemployee.GrossSales:C}");
+            Console.WriteLine($"Commission rate {bpemployee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {bpemployee.Earnings():C}");
+            Console.WriteLine($"Base salary {bpemployee.BaseSalary:C}");
+
+
+            bpemployee.BaseSalary = 1000.0M;
+
+            Console.WriteLine("\nUpdated bpemployee information obtained by ToString() method:\n");
+            Console.WriteLine(bpemployee);
+            Console.WriteLine($"earnings {bpemployee.Earnings():C}");
+
+        }
+    }
+}
